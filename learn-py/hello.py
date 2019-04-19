217d6d8b0423a9fcb5bf5dc47ebeac472ddfe36b
libin <978873512@qq.com>
2019-04-19 16:29:24 +0800

    py

--- learn-py/hello.py
+++ learn-py/hello.py
@@ -9,3 +9,10 @@ if True:
     print("Answer:Yes")
 else:
     print("Answer:No")
+
+item_one = "121"
+item_two = "sad"
+total = item_one + \
+    item_two
+print(total)
+raw_input("按下 enter 键退出，其他任意键显示...\n")
